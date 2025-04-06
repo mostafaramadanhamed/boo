f422a863b9754d9d6426bfb239de15d177304965
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-04-06 10:43:16 +0200

    - initialize cubit and states

---
+++ lib/controllers/cubit/counter_cubit.dart
@@ -0,0 +1,10 @@
+
+import 'package:boo/controllers/cubit/counter_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit():super(const CounterInit());
+
+
+}
