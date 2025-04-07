76df87a4e5290bf2c2c57efca567160d27378038
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-04-07 11:57:49 +0200

    added cubit functions

--- lib/controllers/cubit/counter_cubit.dart
+++ lib/controllers/cubit/counter_cubit.dart
@@ -6,5 +6,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit():super(const CounterInit());
 
+// increment method to increase the count by 1
+  void increment(){
+    emit(CounterUpdate(state.count + 1));    
+  }
+// decrement method to decrease the count by 1
+  void decrement() {
+    emit(CounterUpdate(state.count - 1));
+  }
+// reset method to set the count to 0
+  void reset() {
+    emit(const CounterInit());
+  }
 
 }
