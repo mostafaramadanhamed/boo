6b39e99d1bba0c5d7339dd0e3566cf0891b1f3a8
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-04-09 11:10:53 +0200

    added copy with to task model

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -11,6 +11,17 @@ class TaskModel extends Equatable{
     required this.isCompleted,
   });
 
+  TaskModel copyWith({
+    int? id,
+    String? title,
+    bool? isCompleted,
+  }) {
+    return TaskModel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
   @override
   List<Object?> get props => [id, title, isCompleted];
 }
