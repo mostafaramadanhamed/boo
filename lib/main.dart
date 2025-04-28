c2085a930b39f809bf9785248f92e435a9cda492
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2025-04-28 10:58:24 +0300

    bloc consumer

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:path_provider/path_provider.dart';
+import 'controllers/cubit/login_cubit.dart';
 
-import 'controllers/cubit/task_bloc.dart';
-import 'controllers/cubit/task_event.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory:
-        HydratedStorageDirectory((await getTemporaryDirectory()).path),
-  );
+void main() {
   runApp(const MyApp());
 }
 
@@ -26,86 +17,58 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: BlocProvider(
+        create: (context) => LoginCubit(),
+        child: LoginPage(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key, required this.title});
+class LoginPage extends StatelessWidget {
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
 
-  final String title;
-  final TextEditingController controller = TextEditingController();
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: BlocProvider(
-        create: (context) => TaskBloc(),
-        child: BlocListener<TaskBloc,TaskState>(
-          listener: (context, state) {
-           if (state is UpdateTask) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Text('Task added successfully'),
-                  duration: Duration(seconds: 2),
+      appBar: AppBar(title: const Text("Login")),
+      body: BlocConsumer<LoginCubit, LoginState>(
+        listener: (context, state) {
+          if (state is LoginFailure) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(state.message)));
+          } else if (state is LoginSuccess) {
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Login Sucess")));
+          }
+        },
+        builder: (context, state) {
+          return Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                TextField(
+                  controller: usernameController,
+                  decoration: const InputDecoration(labelText: "Username"),
                 ),
-              );
-           
-            }
-
-          },
-          child: BlocBuilder<TaskBloc, TaskState>(
-            builder: (context, state) {
-              final controllerCubit = context.read<TaskBloc>();
-              return Column(
-                children: [
-                  TextField(
-                    controller: controller,
-                    decoration: const InputDecoration(hintText: 'Enter a task'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (controller.text.isEmpty) return;
-                      controllerCubit.add(AddTaskEvent(controller.text));
-                      controller.clear();
-                    },
-                    child: const Text("Add Task"),
-                  ),
-                  Expanded(
-                      child: ListView.builder(
-                    itemCount: state.tasksList.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(
-                        title: Text(
-                          state.tasksList[index].title,
-                        ),
-                        leading: Checkbox(
-                          value: state.tasksList[index].isCompleted,
-                          onChanged: (value) {
-                            controllerCubit.add(
-                                ToggleTaskEvent(state.tasksList[index].id));
-                          },
-                        ),
-                        trailing: IconButton(
-                          onPressed: () {
-                            controllerCubit.add(
-                                RemoveTaskEvent(state.tasksList[index].id));
-                          },
-                          icon: const Icon(Icons.delete),
-                        ),
-                      );
-                    },
-                  )),
-                ],
-              );
-            },
-          ),
-        ),
+                TextField(
+                    controller: passwordController,
+                    decoration: const InputDecoration(labelText: "Password"),
+                    obscureText: true),
+                const SizedBox(height: 20),
+                state is LoginLoading
+                    ? const CircularProgressIndicator()
+                    : ElevatedButton(
+                        onPressed: () {
+                          context.read<LoginCubit>().login(usernameController.text, passwordController.text);
+                        },
+                        child: const Text("Login"),
+                      ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
